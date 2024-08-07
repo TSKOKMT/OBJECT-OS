8aabc311e03b538ce22f2c9158e0535dd4a0622e
岡本斗志貴 <>
2024-08-07 13:41:22 +0900

    no message

--- src/modules/room/crossRoom.cpp
+++
@@ -1,8 +0,0 @@
-#include "crossRoom.h"
-
-//--------------------------------------------------------------
-void crossRoom::draw(ofRectangle frame_) {
-    
-    ofDrawLine(frame_.getTopLeft(), frame_.getBottomRight());
-    ofDrawLine(frame_.getTopRight(), frame_.getBottomLeft());
-}
--- src/modules/room/ellipseRoom.cpp
+++
@@ -1,7 +0,0 @@
-#include "ellipseRoom.h"
-
-//--------------------------------------------------------------
-void ellipseRoom::draw(ofRectangle frame_) {
-    
-    ofDrawEllipse(frame_.getCenter().x, frame_.getCenter().y, frame_.width, frame_.height);
-}
